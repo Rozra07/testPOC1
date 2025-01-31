ed584ce84060b4579be8223e02e1e9741228e253
Rozra07 <h22099@astra.xlri.ac.in>
2025-01-31 13:41:52 +0530

    Update TestPOCAttPre.py

--- TestPOCAttPre.py
+++ TestPOCAttPre.py
@@ -56,7 +56,8 @@ def compute_weighted_attrition(employee):
     extreme_factors = 0
 
 
-`    if employee["Gender"] == "Female" and employee["Female Employee Ratio"] <= 15:
+`    if some_condition:
+     if employee["Gender"] == "Female" and employee["Female Employee Ratio"] <= 15:
         score += 30
         extreme_factors += 1  
     if employee["Hasn't been promoted"] >= 2 * employee["Minimum Promotion Cycle"]:
